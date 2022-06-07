619b59c2e7afc7d2a1b68e5d0f54390129f07e3f
roey ben arieh <roey280404@gmail.com>
2022-06-07 23:12:23 +0300

    Initial Commit

---
+++ sgc/sgcApp.swift
@@ -0,0 +1,17 @@
+//
+//  sgcApp.swift
+//  sgc
+//
+//  Created by roey ben arieh on 07/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct sgcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
