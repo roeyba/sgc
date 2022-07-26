07794ad22b14913913f3f0b5defaa3d375dc074c
roey ben arieh <roey280404@gmail.com>
2022-07-26 23:46:14 +0300

    simple UI for Glucometer simulator

--- sgc/Screens/Gluco_simulator_View.swift
+++ sgc/Screens/Gluco_simulator_View.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct Gluco_simulator_View: View {
     var body: some View {
-        Text("SimulatorView")
-            .padding()    }
+        NavigationView{
+            VStack {
+                Spacer()
+                Button("press up") {
+                    print("pressed up")
+                }
+                
+                Spacer()
+                Button("press middle") {
+                    print("pressed middle")
+                }
+                .padding(.all, 10.0)
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/3/*@END_MENU_TOKEN@*/)
+                .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                
+                Spacer()
+                Button("press down") {
+                    print("pressed down")
+                }
+                
+                Spacer()
+            }
+            .navigationTitle("manual controler")
+        }
+    }
 }
 
 struct Gluco_simulator_View_Previews: PreviewProvider {
