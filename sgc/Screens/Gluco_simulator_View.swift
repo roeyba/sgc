d96c1ed9e05610d3deea5dcb03a510b24dbc5fc8
roey ben arieh <roey280404@gmail.com>
2022-07-26 12:23:39 +0300

    adding app screens

---
+++ sgc/Screens/Gluco_simulator_View.swift
@@ -0,0 +1,20 @@
+//
+//  Gluco_simulator_View.swift
+//  sgc
+//
+//  Created by roey ben arieh on 26/07/2022.
+//
+
+import SwiftUI
+
+struct Gluco_simulator_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Gluco_simulator_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Gluco_simulator_View()
+    }
+}
---
+++ sgc/Screens/InjectView.swift
@@ -0,0 +1,20 @@
+//
+//  InjectView.swift
+//  sgc
+//
+//  Created by roey ben arieh on 26/07/2022.
+//
+
+import SwiftUI
+
+struct InjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InjectView_Previews: PreviewProvider {
+    static var previews: some View {
+        InjectView()
+    }
+}
